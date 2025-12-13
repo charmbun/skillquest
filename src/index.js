ffd77c231716a8230b2e83c4b6a570d0628f4f65
chaiiiiy <clepilepsia@gmail.com>
2025-12-14 01:41:38 +0800

    Fix: Define HtmlWebPackPlugin in webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-// app startup
-// initialize state
-// call render functions
+import {
+  addSkillQuest,
+  addSubQuest,
+  addTask,
+  getAllSkillQuests,
+} from "./model/state";
+// import { render } from "./ui/render";
+import "./styles/main.css";
+
+// initial data (temporary)
+addSkillQuest("Learn JavaScript");
+addSkillQuest("Learn Python");
+
+// initial render
+// render();
