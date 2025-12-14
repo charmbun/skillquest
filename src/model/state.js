df291e690382e07623b92fc46d645bb45f81db8d
chaiiiiy <clepilepsia@gmail.com>
2025-12-14 12:09:25 +0800

    Fix: addX functions return what it adds

--- src/model/state.js
+++ src/model/state.js
@@ -6,15 +6,21 @@ const state = {
   skillQuests: [],
 };
 
+/* STATE WRITE FUNCTIONS */
+
 function addSkillQuest(title) {
   const quest = createSkillQuest(title);
+
   state.skillQuests.push(quest);
+  return quest;
 }
 
 function addSubQuest(skillQuestId, title) {
   const quest = state.skillQuests.find((quest) => quest.id === skillQuestId);
   const sub = createSubQuest(title);
+
   quest.subquests.push(sub);
+  return sub;
 }
 
 function addTask(subQuestId, title, description, targetDate, priority) {
@@ -23,12 +29,21 @@ function addTask(subQuestId, title, description, targetDate, priority) {
     subquests.push(...quest.subquests);
   });
   const subquest = subquests.find((sq) => sq.id === subQuestId);
-
   const task = createTask(title, description, targetDate, priority);
+
   subquest.tasks.push(task);
+  return task;
 }
 
-// function getSkillQuests() {}
+/* STATE READ FUNCTIONS */
+
+function getAllSkillQuests() {
+  return state.skillQuests;
+}
+
+function getSkillQuestbyId(id) {}
+function getSubQuestbyId(id) {}
+
 // function removeSkillQuest(id) {}
 // function removeSubQuest(skillQuestId, subQuestId) {}
 // function toggleTask(taskId) {}
