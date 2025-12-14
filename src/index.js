b15dc07b519dc72a02adb7476f9357a2e9f6cc04
chaiiiiy <clepilepsia@gmail.com>
2025-12-14 21:17:29 +0800

    Add initial test data in index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import {
   addSubQuest,
   addTask,
   getAllSkillQuests,
-} from "./model/state";
-// import { render } from "./ui/render";
+  getSkillQuestbyId,
+  getSubQuestbyId,
+} from "./state/state.js";
+import { render } from "./ui/render.js";
 import "./styles/main.css";
 
-// initial data (temporary)
-addSkillQuest("Learn JavaScript");
-addSkillQuest("Learn Python");
+const quest = addSkillQuest("Web Programming");
+console.log("Quest: ", quest);
+console.log("Quest given ID: ", getSkillQuestbyId(quest.id));
 
-// initial render
-// render();
+const sub = addSubQuest(quest.id, "JavaScript");
+console.log("Subquest: ", sub);
+console.log("Subquest given ID: ", getSubQuestbyId(sub.id));
+
+const task = addTask(sub.id, {
+  title: "Learn variables",
+  description: "...",
+  targetDate: "2025-01-01",
+  priority: "high",
+});
+console.log("Task: ", task);
+
+const quest2 = addSkillQuest("Nihonggo");
+
+const allQuest = getAllSkillQuests();
+console.log("All Quests: ", allQuest);
+
+render();
